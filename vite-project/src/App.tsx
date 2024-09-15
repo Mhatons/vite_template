c7cd3e8435164c09c5d3620833ee311a6950d7a1
Mhatons <mhatons@gmail.com>
2024-09-15 14:53:43 +0100

    initialised vite/react template with typescript and tailwind integrations

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-[#ddd] py-4 text-center">
+      <h1 className="text-2xl font-bold capitalize">
+        this is a vite/react template with typescript and tailwind css integrations
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // This path should point to all your template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
